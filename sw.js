8476dc196bb2a46a81901b46a8b5a130b3d3684d
OmarRPL06 <omarperezrl38@gmail.com>
2022-10-18 21:23:16 -0500

    commit-1.0.2: se agrego archivo manifest.json

--- sw.js
+++ sw.js
@@ -1,9 +1,12 @@
+// imports
+importScripts('js/sw-utils.js');
+
 const STATIC_CACHE = 'static-v1';
 const DYNAMIC_CACHE = 'dynamic-v1';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
 const APP_SHELL = [
-    '/',
+    // '/',
     'index.html',
     'css/style.css',
     'img/favicon.ico',
@@ -12,6 +15,8 @@ const APP_SHELL = [
     'img/avatars/spiderman.jpg',
     'img/avatars/thor.jpg',
     'img/avatars/wolverine.jpg',
+    'js/app.js',
+    'js/sw-utils.js'
 ];
 
 const APP_SHELL_INMUTABLE = [
@@ -29,4 +34,31 @@ self.addEventListener('install',e =>{
         cache.addAll(APP_SHELL_INMUTABLE))
 
     e.waitUntil(Promise.all([cacheStatic,cacheInmutable]))
-})
+});
+
+self.addEventListener('activate',e=>{
+    const respuesta=caches.keys().then(keys=>{
+        keys.forEach(key=>{
+            if(key!=STATIC_CACHE && key.includes('static')){
+                return caches.delete(key);
+            }
+        })
+    })
+    e.waitUntil(respuesta);
+});
+
+// estrategia caché only
+self.addEventListener('fetch', e=>{
+    const respuesta = caches.match(e.request).then(resp=>{
+        if(resp){
+            return resp;
+        }else{
+            return fetch(e.request).then(newResp=>{
+                return actualizaCacheDinamico(DYNAMIC_CACHE, e.request, newResp);
+            })
+        }
+    });
+    e.respondWith(respuesta);
+});
+
+
